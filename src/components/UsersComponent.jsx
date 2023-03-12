d4fba974023176d523acb8a362cfcd0f19cd0fd5
Alien Coder <paidcodecamp@gmail.com>
2023-03-12 16:40:25 +0530

    added redux-saga, called apis and updated apis async

--- src/components/UsersComponent.jsx
+++ src/components/UsersComponent.jsx
@@ -6,29 +6,22 @@ import Card from "./CardComponent";
 const UsersComponent = () => {
   const dispatch = useDispatch();
   const users = useSelector((state) => state.users.users);
+  const loading = useSelector((state) => state.users.loading);
+  const error = useSelector((state) => state.users.error);
 
   useEffect(() => {
-    dispatch(
-      getUsers([
-        {
-          id: 1,
-          name: "Leanne Graham",
-          company: {
-            name: "Romaguera",
-            catchPhrase: "Multi-layered client-server neural-net",
-          },
-        },
-      ])
-    );
+    dispatch(getUsers());
   }, [dispatch]);
 
   return (
     <>
+      {loading && <p>Loading...</p>}
       {users.length > 0 &&
         users.map((user) => {
           return <Card user={user} key={user.id} />;
         })}
-      {users.length === 0 && <p>No users available!</p>}
+      {users.length === 0 && !loading && <p>No users available!</p>}
+      {error && !loading && <p>{error}</p>}
     </>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,15 @@
+import createSagaMiddleware from "@redux-saga/core";
 import { compose, createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
-
+import rootSaga from "./sagas";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware()));
+const sagaMiddleware = createSagaMiddleware();
 
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
 export default store;
